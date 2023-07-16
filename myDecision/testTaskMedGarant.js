935be064404b78bed8f897203285b4ff61b66e50
AlbShar <sharipov.albert1991@gmail.com>
2023-07-16 22:09:56 +0500

    update myDecision.js file

--- myDecision/testTaskMedGarant.js
+++ myDecision/testTaskMedGarant.js
@@ -15,7 +15,8 @@ const toStrTime = (timeNum) => {
   const hours = Math.trunc(timeNum / 60);
   const fillHours = hours < 10 ? `0${hours}` : hours;
   const minutes = timeNum - hours * 60;
-  const fullMinutes = minutes === 0 ? `${minutes}0` : minutes;
+  const fullMinutes =
+    minutes === 0 ? `${minutes}0` : minutes < 10 ? `0${minutes}` : minutes;
   return `${fillHours}:${fullMinutes}`;
 };
 
